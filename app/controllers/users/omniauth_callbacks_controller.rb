3409c783cb440c06635283849c64505e6d4a49ef
Andrea Schiavini <metalelf0@gmail.com>
2015-01-26 16:20:09 +0100

    typo

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,7 +6,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication # this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => 'redbooth') if is_navigational_format?
     else
-      session['devise.facebook_data'] = request.env['omniauth.auth']
+      session['devise.redbooth_data'] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
     end
   end
