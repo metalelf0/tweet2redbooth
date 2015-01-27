4a274c3d11e1a7bb162fb361d34409479e63919f
Andrea Schiavini <metalelf0@gmail.com>
2015-01-27 10:45:09 +0100

    Offending line

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication # this will throw if @user is not activated
-      session['devise.redbooth_data'] = request.env['omniauth.auth']
+      # session['devise.redbooth_data'] = request.env['omniauth.auth']
       set_flash_message(:notice, :success, :kind => 'redbooth') if is_navigational_format?
     else
       session['devise.redbooth_data'] = request.env['omniauth.auth']
